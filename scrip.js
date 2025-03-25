24eb4fd83c96645c64c2edd4ec1c0e6f06af71ca
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-03-25 19:33:31 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,8 @@
+let students = {}
+
+students.name = prompt('Ismingizni kiriting');
+students.id = prompt("Space id yingizni kiriting");
+students.pasword = prompt("space paswordgizni kiriting")
+
+
+console.log(students)
